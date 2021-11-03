d041f56eb79c21bef3df2cc437b896a2b7d062ee
Jérémy Bouche <jeremy.bouche@swile.co>
2021-11-03 19:23:21 +0100

    add ping route

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -4,6 +4,7 @@ import { middleWareReviews, reviews } from '@controllers/webhooks/reviews'
 
 const router = express.Router()
 
+router.get('/ping', (_, res) => res.status(200).json())
 router.post('/webhooks/pull-request', middleWarePullRequest, pullRequest)
 router.post('/webhooks/reviews', middleWareReviews, reviews)
 
