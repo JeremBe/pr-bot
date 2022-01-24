331632e8448ca48240230bc4b72b71930c824710
Jérémy Bouche <jeremy.bouche@swile.co>
2022-01-25 00:50:52 +0100

    add logs

--- app/core/notify.ts
+++ app/core/notify.ts
@@ -15,14 +15,17 @@ export async function notifyPullRequest(pullRequest: PullRequest, webhook: Webho
   switch (pullRequest.status) {
     case 'closed':
       if (webhook.pull_request.merged_at) {
+        console.log('[app/core/notify#notifyPullRequest] notify pull request merged')
         await notifyPullRequestMerged(pullRequest)
       }
 
       break
     case 'opened':
       const isCreated = pullRequest.updatedAt.getTime() - pullRequest.createdAt.getTime() < 5
+      console.log('[app/core/notify#notifyPullRequest] isCreated', isCreated)
 
       if (isCreated) {
+        console.log('[app/core/notify#notifyPullRequest] notify pull request created')
         await notifyPullRequestCreated(pullRequest)
       }
 
