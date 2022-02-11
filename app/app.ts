02c43f38cf7724e37d0ff90e155a6c5c1af9d5fb
Jérémy Bouche <jeremy.bouche@swile.co>
2022-02-11 22:31:49 +0100

    feat: :sparkles: Save raw JSON body in req.rawBody

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import cors from 'cors'
 
 import routes from '@routes/index'
+import { rawBodySaver } from '@utils/rawBodySaver'
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -14,7 +15,11 @@ app.use(
   }),
 )
 
-app.use(express.json())
+app.use(
+  express.json({
+    verify: rawBodySaver,
+  }),
+)
 
 /**
  * ROUTES API
