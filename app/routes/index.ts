7aaf7495d4fe840c290c7dfa1a5876b627ca57c7
Jérémy Bouche <jeremy.bouche@swile.co>
2022-02-12 15:40:49 +0100

    feat: :rocket: Slack events api

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -5,10 +5,14 @@ import { reviewsController } from '@controllers/webhooks/reviews'
 import { adminPullRequestControllerIndex } from '@controllers/admin/pull-request'
 import { adminReviewsControllerIndex } from '@controllers/admin/reviews'
 import { githubMiddleware } from '@middlewares/github'
+import { slackEvents } from '@controllers/slack/events'
 
 const router = express.Router()
 
 router.get('/ping', (_, res) => res.status(200).json())
+
+// SLACK ROUTES
+router.use('/slack/events', slackEvents.requestListener())
 
 // WEBHOOKS
 router.post('/webhooks/pull-request', githubMiddleware, pullRequestController)
