bfb0505fee9965880e3a4201cc00055ecc7eec23
Jérémy Bouche <jeremy.bouche@swile.co>
2022-02-12 14:26:33 +0100

    feat: :rocket: add github middleware on webhoos routes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -4,14 +4,15 @@ import { pullRequestController } from '@controllers/webhooks/pull-request'
 import { reviewsController } from '@controllers/webhooks/reviews'
 import { adminPullRequestControllerIndex } from '@controllers/admin/pull-request'
 import { adminReviewsControllerIndex } from '@controllers/admin/reviews'
+import { githubMiddleware } from '@middlewares/github'
 
 const router = express.Router()
 
 router.get('/ping', (_, res) => res.status(200).json())
 
 // WEBHOOKS
-router.post('/webhooks/pull-request', pullRequestController)
-router.post('/webhooks/reviews', reviewsController)
+router.post('/webhooks/pull-request', githubMiddleware, pullRequestController)
+router.post('/webhooks/reviews', githubMiddleware, reviewsController)
 
 // ADMIN ROUTES
 router.get('/admin/pull-requests', adminPullRequestControllerIndex)
