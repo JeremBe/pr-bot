fbd6fd1269223e2cd8b1aa570c55830374f54458
Jerem <jeremy.bouche@swile.co>
2022-02-12 16:33:12 +0100

    feat: :rocket: Slack events api (#28)
    * feat: :rocket: Slack events api
    
    * refactor: :zap: recfactor slack webClient implementation

---
+++ app/controllers/slack/events.ts
@@ -0,0 +1,30 @@
+import { createEventAdapter } from '@slack/events-api'
+
+import { config } from '@config'
+import { getWebClient } from '@core/slack'
+
+const webClient = getWebClient()
+
+export const slackEvents = createEventAdapter(config.slack.signingSecret)
+
+slackEvents.on('app_mention', async (event) => {
+  console.log('[app/controllers/slack/events.ts#app_mention] Event', { event })
+  try {
+    const mentionResponseBlock = { ...messageJsonBlock, channel: event.channel }
+    await webClient.chat.postMessage(mentionResponseBlock)
+  } catch (error) {
+    console.log('[app/controllers/slack/events.ts#app_mention] Error', { error })
+  }
+})
+
+const messageJsonBlock = {
+  blocks: [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'Hello, thanks for calling me 🎉',
+      },
+    },
+  ],
+}
