4b75e0c152326d6024b49858600f7170d07e63c6
Jerem <jeremy.bouche@swile.co>
2022-02-12 02:10:30 +0100

    refactor: :sparkles: Manage teamId across app (#26)

--- app/controllers/admin/reviews.ts
+++ app/controllers/admin/reviews.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express'
 import { database } from '@core/database'
 
 export async function adminReviewsControllerIndex(_: Request, res: Response) {
-  const reviews = await database.reviewer.findMany()
+  const reviews = await database.review.findMany()
 
   res.status(200).json({
     reviews,
