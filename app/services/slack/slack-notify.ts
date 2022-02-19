ff72db6822a801fddbc5b96f3654bef677e4f55a
Jérémy Bouche <jeremy.bouche@swile.co>
2022-02-19 22:13:51 +0100

    feat: :sparkles: notify multiple channels

--- app/services/slack/slack-notify.ts
+++ app/services/slack/slack-notify.ts
@@ -1,33 +1,45 @@
-import { PullRequest, Review } from '@prisma/client'
+import { PullRequest, Review, SlackChannelSubscription } from '@prisma/client'
 
 import { getWebClient } from '@services/slack/slack'
 import { blockPullRequestCreated, blockPullRequestMerged, blockReview } from '@core/slack-blocks'
 
 const webClient = getWebClient()
 
-export async function notifyPullRequestCreated(pullRequest: PullRequest) {
+export async function notifyPullRequestCreated(pullRequest: PullRequest, channels: SlackChannelSubscription[]) {
   console.log('[app/services/slack/slack-notify#notifyPullRequestCreated]')
   console.log(pullRequest)
 
   const block = blockPullRequestCreated(pullRequest)
 
-  await webClient.chat.postMessage({ blocks: block, text: 'message', channel: 'C02N7GB1JUX' })
+  await Promise.all(
+    channels.map((channel) =>
+      webClient.chat.postMessage({ blocks: block, text: 'message', channel: channel.channelId }),
+    ),
+  )
 }
 
-export async function notifyPullRequestMerged(pullRequest: PullRequest) {
+export async function notifyPullRequestMerged(pullRequest: PullRequest, channels: SlackChannelSubscription[]) {
   console.log('[app/services/slack/slack-notify#notifyPullRequestMerged]')
   console.log(pullRequest)
 
   const block = blockPullRequestMerged(pullRequest)
 
-  await webClient.chat.postMessage({ blocks: block, text: 'message', channel: 'C02N7GB1JUX' })
+  await Promise.all(
+    channels.map((channel) =>
+      webClient.chat.postMessage({ blocks: block, text: 'message', channel: channel.channelId }),
+    ),
+  )
 }
 
-export async function notifyReviews(pullRequest: PullRequest, reviews: Review[]) {
+export async function notifyReviews(pullRequest: PullRequest, reviews: Review[], channels: SlackChannelSubscription[]) {
   console.log('[app/services/slack/slack-notify#notifyReviews]')
   console.log(pullRequest)
 
   const block = blockReview(pullRequest, reviews)
 
-  await webClient.chat.postMessage({ blocks: block, text: 'message', channel: 'C02N7GB1JUX' })
+  await Promise.all(
+    channels.map((channel) =>
+      webClient.chat.postMessage({ blocks: block, text: 'message', channel: channel.channelId }),
+    ),
+  )
 }
